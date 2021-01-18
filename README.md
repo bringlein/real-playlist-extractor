e0155148a72a7771e35118879b8a1794c11706e8
Burkhard Ringlein <mail@burkhard.engineer>
2021-01-18 21:37:58 +0100

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# real-playlist-extractor
-A python script to extract meta data of BlueRay Real Playlist (.rpls) folder. 
+Real Playlist Metadata Readout
+===============================
+
+A python script to extract meta data of Blu-Ray Real Playlist (`.rpls`) folder.
+
+Background
+-------------
+
+RPLS is used by some vendors for the BDAV [(Blu-ray Disc Audio/Visual)](https://en.wikipedia.org/wiki/Blu-ray#Application_format) format of Blu-Ray discs to store the metadata of the recorded videos.
+
+BDAV is intended to be used by consumer to safe recorded videos, including from personal camcorders. 
+But it is also used by some DVR/AVR vendors (e.g. Panasonic) as file system for the external HDD.
+
+
+The BDAV [file structure](https://en.wikipedia.org/wiki/Blu-ray#Directory_and_file_structure) is as follows:
+```
+BDAV/
+├── info.bdav
+├── menu.something
+├── CLIPINF
+│   ├── 00001.clpi
+│   ├── 00002.clpi
+│   ├── 00003.clpi
+│   ├── .....
+├── PLAYLIST
+│   ├── 00001.rpls
+│   ├── 00002.rpls
+│   ├── 00003.rpls
+│   ├── .....
+└── STREAM
+    ├── 00001.m2ts
+    ├── 00002.m2ps
+    ├── 00003.m2ps
+    ├── .....
+```
+
+- `PLAYLIST` contains the metadata of the videos
+- `STREAM`  contains the videos
+- (`CLIPINF` some scene information etc...not relevant for this script).
+
+
+The file file names *do not match*! I.e. it is *not* required that `0001.rpls` belongs to `0001.m2ps`! 
+
+However, the `.rpls` file contains the name of the `STREAM` file that it describes. Hence, a short script can extract the name (and other metadata) of the `.rpls` files and match it to the video files. 
+
+
+The script `main.py` extracts the title of the playlist file, matches it to the corresponding video file, stores the title as id3 tag in the file, and finally renames the file.
+
+Usage
+-------
+
+```
+$ python3.8 main.py /path/to/BDAV/PLAYLIST /path/to/BDAV/STREAM/or-other-video-folder /path/to/processed-and-renamed-output-dir
+```
+For further details, please see my blog.
+
+### License
+
+Apache 2.0
+
+
+### Requirements
+
+python 3.8 (other python 3.4+ versions may work as well).
+
+
---
+++ main.py
@@ -0,0 +1,113 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import os
+import sys
+import glob
+import string
+from datetime import datetime
+
+
+_album_name_ = "My Recordings"
+
+
+def copy_and_rename_mp4(nr, source_mp4, new_name, target_dir):
+    new_file_name = nr + "_" + new_name.replace(" ", "_").replace(".", "-") + ".m4v"
+    new_path = os.path.join(target_dir, new_file_name)
+    today = datetime.today().strftime('%Y-%m-%d')
+    # if you want to convert the files instead of copying, alter the string below
+    # e.g. for m2ts/m2ps to mp4 use the following:  -vcodec libx264 -preset veryfast -crf 20 -acodec ac3 -vf "yadif"
+    cmd = '/usr/bin/ffmpeg -loglevel quiet -i {} -codec copy -metadata title=\"{}\" -metadata album=\"{}\" ' \
+          '-metadata copy_date=\"{}\" {}'.format(
+        source_mp4, new_name, _album_name_, today, new_path)
+    print(cmd)
+    os.system(cmd)
+
+
+def extract_rpls_metadata(file_path):
+    bytes = open(file_path, "rb").read()
+
+    length_byte = 0
+    text_start_byte = 0
+    if bytes[87] == 0 and bytes[88] > 0:
+        length_byte = 88
+        text_start_byte = 89
+    else:
+        print("INVALID RPL FORMAT of file {}\n".format(file_path))
+        exit(2)
+
+    name = bytes[text_start_byte:(text_start_byte+bytes[length_byte])]
+    # name_orig = name.decode("utf-8")
+    name_orig = name.decode("latin-1")
+
+    # check for expected chars
+    if bytes[0x732] != 0 and bytes[0x733] != 0x14:
+        print("FATAL: {} has not expected bytes at addresses 0x732 and 0x733!\n".format(file_path))
+        exit(5)
+
+    fc = bytes[0x734:0x734+9].decode("utf-8")
+    # ft = fc[-4:].lower()
+    # video_file_name = fc[:-4] + "." + ft
+    mp4_file_name = fc[:-4] + ".mp4"
+
+    print("found {} for {} in {}".format(name_orig, mp4_file_name, os.path.basename(file_path)))
+
+    # printable = set(string.printable)
+    valid_chars = "-_.() %s%säöüïß" % (string.ascii_letters, string.digits)
+
+    # make sure only "safe" characters are used for the metadata / filename
+    ret = ''.join(filter(lambda x: x in valid_chars, name_orig))
+    return ret, mp4_file_name
+
+
+def main(rpls_dir, mp4_dir, target_dir):
+    # print("working on {}".format(rpls_dir))
+    rpl_files = glob.glob(rpls_dir + "/*.rpls")
+    rpl_files.sort()
+    print(rpl_files)
+
+    mp4_files = glob.glob(mp4_dir + "/*.mp4")
+    mp4_files.sort()
+    print(mp4_files)
+
+    if len(rpl_files) != len(mp4_files):
+        print("ERROR: content of directories aren't matching!\n")
+        exit(1)
+
+    for i in range(0, len(rpl_files)):
+        cur_rpl = rpl_files[i]
+        new_name, mp4_file_name = extract_rpls_metadata(cur_rpl)
+        cur_mp4 = os.path.join(mp4_dir, mp4_file_name)
+        if cur_mp4 in mp4_files:
+            di = mp4_files.index(cur_mp4)
+            del mp4_files[di]
+        else:
+            print("FATAL: extracted file that does not exist: {} from {}.\n".format(cur_mp4, cur_rpl))
+            exit(6)
+        copy_and_rename_mp4("{:04d}".format(i), cur_mp4, new_name, target_dir)
+
+    print("\n\tDONE.\n")
+    if len(mp4_files) > 0:
+        print("The following mp4 files were not processed:\n {}".format(mp4_files))
+    else:
+        print("All files of {} were processed.".format(mp4_dir))
+
+
+if __name__ == '__main__':
+    # usage: python3.8 main.py /path/to/BDAV/PLAYLIST /path/to/BDAV/STREAM/or-other-video-folder /path/to/processed-and-renamed-output-dir
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
+
